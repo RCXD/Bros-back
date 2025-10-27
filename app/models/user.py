16a8ea15c4e5571207db4b1eb41477a55db5b54d
WinLakeLee <lst2413@gmail.com>
2025-10-27 16:41:06 +0900

    주소, 프로필 이미지 추가
    주소, 프로필 이미지 컬럼 추가

--- app/models/user.py
+++ app/models/user.py
@@ -23,6 +23,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    profile_img = db.Column(db.String(255))
     nickname = db.Column(db.String(50))
     created_at = db.Column(db.DateTime, default=datetime.now)
     last_login = db.Column(db.DateTime, default=datetime.now)
