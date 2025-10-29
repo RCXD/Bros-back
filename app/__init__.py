b3648cf5629ef2c7e28e592578d02b1137ee26cc
yeonjaegit <keo0713@naver.com>
2025-10-29 11:50:34 +0900

    Mention: get요청 라우트 생성
    Mention: get요청 라우트 사용자용(등록, user_id로 언급 조회)
    
    __init__.py : mention 블루프린트 추가
    mention : 모델 컬럼 추가(is_checked) = 멘션 알림 확인 했는지 여부
    
    모든 모델 get요청 라우터 작업 진행중

--- app/__init__.py
+++ app/__init__.py
@@ -18,9 +18,11 @@ def create_app():
     from .blueprints.auth import bp as auth_bp
     from .blueprints.post import bp as post_bp
     from .blueprints.reply import bp as reply_bp
+    from .blueprints.mention import bp as mention_bp
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(post_bp, url_prefix="/post")
     app.register_blueprint(reply_bp, url_prefix="/reply")
+    app.register_blueprint(mention_bp, url_prefix="/mention")
 
     return app
