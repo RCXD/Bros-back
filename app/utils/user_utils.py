128aec7da53de8aacb486211c6ba220db91bdb2b
yeonjaegit <keo0713@naver.com>
2025-11-04 11:09:00 +0900

    auth.py 블루프린트 라우트 추가
    auth.py : 회원 수정 / 삭제 라우트 추가,
    user 모델에 phone 컬럼 추가
    __init__.py : mypage블루프린트 삭제(auth.py에 통합함)
    user_utils.py  : phone번호 검증 함수 추가

--- app/utils/user_utils.py
+++ app/utils/user_utils.py
@@ -2,6 +2,7 @@ from flask_jwt_extended import create_access_token, create_refresh_token, get_cs
 from flask import jsonify
 from ..extensions import db
 from ..models import User
+import re
 
 
 def token_provider(user_id):
@@ -40,3 +41,8 @@ def user_to_dict(user):
         "oauth_type": user.oauth_type.name,
         "follower_count": user.follower_count,
     }
+
+PHONE_REGEX = re.compile(r"^0\d{1,2}-?\d{3,4}-?\d{4}$")
+
+def is_valid_phone(phone: str) -> bool:
+    return bool(PHONE_REGEX.match(phone))
