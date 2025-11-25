15bc7a077556fabe85d005a6ca88dd30db17f21b
Ch K <tpxdkss@gmail.com>
2025-11-26 08:32:42 +0900

    페이지네이션 응답 일관성 수정

--- apps/place/views.py
+++ apps/place/views.py
@@ -458,6 +458,8 @@ def list_places():
                 "pages": pagination.pages,
                 "page": page,
                 "per_page": per_page,
+                "has_next": pagination.has_next,
+                "has_prev": pagination.has_prev,
             }
         ),
         200,
